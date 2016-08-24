97f4dbb2ba27eaaf8e267ec47146946719521581
philipp <philipp@uberall.com>
2016-08-24 18:23:15 +0200

    make the plugin itself runnable to test stuff easier

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,23 @@
+import errbuddy.ErrbuddyLogAppender
+import errbuddy.ErrbuddyService
+import grails.core.GrailsApplication
+import org.springframework.beans.factory.support.AbstractBeanDefinition
+import org.springframework.beans.factory.support.GenericBeanDefinition
+
 class BootStrap {
 
+    ErrbuddyService errbuddyService
+    GrailsApplication grailsApplication
+
     def init = { servletContext ->
+        def errbuddyLogAppender = new GenericBeanDefinition(beanClass: ErrbuddyLogAppender, autowireMode: AbstractBeanDefinition.AUTOWIRE_BY_NAME)
+
+        grailsApplication.mainContext.registerBeanDefinition('errbuddyLogAppender', errbuddyLogAppender)
+        def logAppender = grailsApplication.mainContext.getBean('errbuddyLogAppender')
+
+        logAppender.enable()
+        log.error("wattt????")
+        errbuddyService.postDeployment("foo", "bar")
     }
     def destroy = {
     }
